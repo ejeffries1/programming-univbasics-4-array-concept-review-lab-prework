a0f439fca522803d6b2d24570928f43e09b45ba7
Evan Jeffries <ejeffries14@gmail.com>
2019-07-18 11:23:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   array.collect do |int|
     if int = value_to_find
-      return int.index(int)
+      return int.index()
     else
       nil
     end
